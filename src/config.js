2799bf2adececbde0308088636de5df8b1c2e682
Haider <hai.tiwana@gmail.com>
2019-11-13 14:30:16 -0800

    setting up React application

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "haiders-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://gr87tykgjk.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_BEZGE1Q0b",
+        APP_CLIENT_ID: "5tcc68905snnb0vl593okjh7as",
+        IDENTITY_POOL_ID: "92b60f1a-56bf-4f24-9a00-3e1ff250e086"
+    }
+};
